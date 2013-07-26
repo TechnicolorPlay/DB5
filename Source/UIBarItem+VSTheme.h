f0b60ccd93f99d8f86fb22f1af9939c6544aac45
Eric Robinson <ericrobinson@groupneon.com>
2013-07-26 17:45:15 -0400

    add grid methods, category for UIBarItem

---
+++ Source/UIBarItem+VSTheme.h
@@ -0,0 +1,13 @@
+//
+//  UIBarItem+UIBarItem_VSTheme.h
+//  NeonEngine
+//
+//  Created by Eric Robinson on 7/26/13.
+//  Copyright (c) 2013 groupneon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIBarItem (UIBarItem_VSTheme)
+
+@end
---
+++ Source/UIBarItem+VSTheme.m
@@ -0,0 +1,13 @@
+//
+//  UIBarItem+UIBarItem_VSTheme.m
+//  NeonEngine
+//
+//  Created by Eric Robinson on 7/26/13.
+//  Copyright (c) 2013 groupneon. All rights reserved.
+//
+
+#import "UIBarItem+UIBarItem_VSTheme.h"
+
+@implementation UIBarItem (UIBarItem_VSTheme)
+
+@end
